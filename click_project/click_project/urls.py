44c705edd3289a16c0e213813de0e220517a0897
Wahomethegeek <gatwirikelvin@outlook.com>
2023-10-31 16:48:16 +0300

    adding an image for clicking

--- click_project/click_project/urls.py
+++ click_project/click_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Click_project project.
+URL configuration for click_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -17,9 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from click_project.click_app.views import index
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index', index)
 ]
